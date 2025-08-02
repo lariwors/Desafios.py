61af848c2b51c7f2e20187b679c354dd43960e9b
unknown <lariwors13@outlook.com>
2025-08-02 18:48:27 -0300

    Adiciona aulas 13 e 14 - Estruturas de repetição.

---
+++ PycharmProjects/CursoEmVideo-Python/aula014 - Estrutura de repetição WHILE.py
@@ -0,0 +1,27 @@
+c = 1
+while c < 10:
+    print(c)
+    c += 1
+print('FIM')
+
+while c != 0:
+    c = int(input('Digite um valor: '))
+print('Fim')
+
+r = 'S'
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? ')).upper()
+print('FIM')
+
+n = 1
+par = impar = 0
+while n != 0:
+    n = int(input('Digite um valor: '))
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+print('Você digitou {} números pares e {} números ímpares.'.format(par, impar))
+print('FIM')
